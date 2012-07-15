c5a806316e62c7ee8bdbc2d33a44b8dc22a4d84c
Philippe Lewicki <philippe@ofuz.org>
2012-07-15 12:34:39 -0700

    add click event on d3 and experiment d3 interactions

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ meteordraw
 ==========
 
 meteor drawing app
+
+move the content of package in /usr/lib/meteor/packages/
+
+make sure packages folder doesn't stay in your project folder.
+Not usre yet why but meteor doesn't like it.
+
--- meteordraw.js
+++ meteordraw.js
@@ -2,10 +2,14 @@ if (Meteor.is_client) {
   Template.hello.greeting = function () {
     return "Welcome to meteordraw.";
   };
+  Template.hello.drawsomething = function () {
+	  d3.select('#mydrawboard').style("color", "green");
+  };
 
   Template.hello.events = {
     'click input' : function () {
       // template data, if any, is available in 'this'
+      d3.select("body").style("background-color", "black");
       if (typeof console !== 'undefined')
         console.log("You pressed the button");
     }
