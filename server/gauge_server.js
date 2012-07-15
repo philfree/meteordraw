95647c1e15c7ef91cbac36588a7182b279dedd10
Philippe Lewicki <philippe@ofuz.org>
2012-07-15 15:46:42 -0700

    added Graph db object trying to display it

--- server/gauge_server.js
+++ server/gauge_server.js
@@ -2,10 +2,22 @@
 // loading default graphs
 
   Meteor.startup(function () {
+	console.log("will check for Graphs...");
     if (Graphs.find().count() === 0) {
+	  console.log("Graphs is empty adding a few");
       var names = ["Boxes",
-                   "Gauge"];
+                   "Gauge",
+                   "test"];
       for (var i = 0; i < names.length; i++)
         Graphs.insert({name: names[i]});
-    }
+    } else {		
+		var top_graph = Graphs.find({}, {});
+		var count = 0;
+		top_graph.forEach(function (graph) {
+		  console.log("Graph Names " + count + ": " + graph.name);
+		  count += 1;
+		});
+		//Graphs.ForEach(graph) function 
+	}
+
   });
