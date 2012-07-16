43f4acdb1316d9ce59ed1891a0c8dd9ba0249dfa
Philippe Lewicki <philippe@ofuz.org>
2012-07-15 17:53:06 -0700

    added events to select the graph and load / save the circle values on the selected graph

--- client/gauge.js
+++ client/gauge.js
@@ -4,6 +4,7 @@ Circles = new Meteor.Collection("circles");
 console.log("Audrey test");
 
 
+
 if (Meteor.is_client) {
 
   Template.gauge.drawsomething = function () {
@@ -14,16 +15,18 @@ if (Meteor.is_client) {
 
   Meteor.subscribe('graphs', function () {
     if (!Session.get('graph_id')) {
-	 console.log("no graph id value in session, doing nothing for now");
-     var graph = Graphs.findOne({}, {sort: {name: 1}});
-     if (graph)
+	  console.log("no graph id value in session, doing nothing for now");
+      var graph = Graphs.findOne({}, {sort: {name: 1}});
+      if (graph) {
         Router.setGraph(graph._id);
+        Session.set("selected_graph", graph._id);
+	  }
     }
   });
 
 // Always be subscribed to the todos for the selected list.
   Meteor.autosubscribe(function () {
-	  var graph_id = Session.get('graph_id');
+	  var graph_id = Session.get('selected_graph');
 	  if (graph_id)
 		Meteor.subscribe('circles', graph_id);
   });
@@ -47,32 +50,41 @@ if (Meteor.is_client) {
         .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
         .on("mouseout", function(){d3.select(this).style("fill", "white");});
         Circles.insert({
-		          graph_id: Session.get('graph_id'), 
+		          graph_id: Session.get('selected_graph'), 
 		          cx: event.layerX,
 		          cy: event.layerY }) ;
     }
   };
 }
 
-Template.graph_list.circle_pos = function () {
-  var graph_id = this._id;
-  return Circles.find({graph_id: graph_id}, {});
-  //return _.map(this.tags || [], function (tag) {
-  //  return {todo_id: todo_id, tag: tag};
-  //});
-};
+  Template.graph_list.circle_pos = function () {
+    var graph_id = this._id;
+    return Circles.find({graph_id: graph_id}, {});
+  };
 
    Template.graph_list.graph_all = function () {
 	 console.log("template function: graph_all");
      return Graphs.find({}, {});
    };
-
+   
+   Template.graph_list.events = {
+    'click': function () {
+	  console.log("Click event selecting Graph:"+this._id);
+      Session.set("selected_graph", this._id);
+      Router.setGraph(this._id);
+    }
+  };
+  Template.graph_list.selected = function () {
+    return Session.equals("selected_graph", this._id) ? "selected" : '';
+  };
+  
+  
 var GraphRouter = Backbone.Router.extend({
   routes: {
     ":graph_id": "main"
   },
   main: function (graph_id) {
-    Session.set("graph_id", graph_id);
+    Session.set("selected_graph", graph_id);
   },
   setGraph: function (graph_id) {
     this.navigate(graph_id, true);
